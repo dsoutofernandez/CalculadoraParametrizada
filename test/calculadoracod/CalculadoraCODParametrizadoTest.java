c394e624a2aca9eec5ecca4700b85a28b6ef3e12
dsoutofernandez <dsoutofernandez@atanasoft222.danielcastelao.org>
2015-06-01 12:06:58 +0200

    Commit 2 Calculadoras Areas Test Parametrizadas

---
+++ test/calculadoracod/CalculadoraCODParametrizadoTest.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadoracod;
+
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+/**
+ *
+ * @author dsoutofernandez
+ */
+
+    
+    
+
+
+@RunWith(Parameterized.class)
+public class CalculadoraCODParametrizadoTest {
+    
+    private double result;
+    private Modelo mod = new Modelo();
+    
+    
+   @Before
+   public void initialize() {
+   
+   
+   }
+
+    public CalculadoraCODParametrizadoTest(Modelo mod,double result) {
+      this.mod = mod;
+      this.result = result;
+   }
+
+ 
+
+   @Parameterized.Parameters
+   public static Collection primeNumbers() {
+      return Arrays.asList(new Object[][] {
+         { new Modelo (2,2, "suma"),4},
+         { new Modelo (3,1, "resta"),2}
+         
+      });
+   }
+
+   // This test will run 4 times since we have 5 parameters defined
+   @Test
+   public void testPrimeNumberChecker() {
+      System.out.println("Parameterized Number is : " +result);
+      assertEquals(result, CalculadoraCOD.realizaOperacion(mod),0.1);
+   }
+}
+
